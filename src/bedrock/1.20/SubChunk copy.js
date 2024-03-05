f8af9dc0dfd73d542bf622a5b7f0d40ff01977f2
Irkmandeer <niel@pink-elephant.co.za>
2024-03-05 18:00:00 +0200

    Added notes

--- src/bedrock/1.20/SubChunk copy.js
+++ src/bedrock/1.20/SubChunk copy.js
@@ -4,6 +4,40 @@ const PalettedStorage = require('../common/PalettedStorage')
 
 class SubChunk120 extends SubChunk118 {
 
+  loadPalettedBlocks2(storageLayer, stream, bitsPerBlock, format) {
+
+    if ((format === StorageType.Runtime) && (bitsPerBlock === 0)) {
+      this.palette[storageLayer] = []
+      this.blocks[storageLayer] = new PalettedStorage(1)
+      // const stateId = stream.readVarInt() >> 1
+      // this.addToPalette(storageLayer, stateId)
+
+      const block_runtime_id = stream.readVarInt() >> 1
+      var stateId = this.registry.blockStatesByRuntimeId[block_runtime_id]
+
+
+      if (!this.registry.blockStates[stateId]) {
+
+
+
+
+        // console.log(this.registry.blocksByName.unknown.defaultState)
+        console.log('addToPalette', block_runtime_id, stateId)
+
+        //stateId = this.registry.blockStatesByRuntimeId[-2]
+        //stateId = this.registry.blocksByName.unknown.defaultState
+        // stateId = this.registry.blocksByName.bedrock.defaultState
+
+      }
+      
+      this.addToPalette(storageLayer, stateId)
+
+      return
+    }
+    return super.loadPalettedBlocks(...arguments)
+  }
+
+
   loadPalettedBlocks(storageLayer, stream, bitsPerBlock, format) {
 
     //TODO: blockStatesByRuntimeId
@@ -27,9 +61,6 @@ class SubChunk120 extends SubChunk118 {
 
       const block_runtime_id = stream.readZigZagVarInt()
       const stateId = this.registry.blockStatesByRuntimeId[block_runtime_id]
-
-      //-2 unknown is not working as expected, pathfinder sees this as air.
-      //TODO: load behaviour packs.
       const block = this.registry.blockStates[stateId] ?? this.registry.blockStates[this.registry.blockStatesByRuntimeId[-2]]
       this.palette[storageLayer][i] = { stateId, ...block, count: 0 }
     }
